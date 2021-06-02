fbf582cd6d311e4456477855c9a4b616ff5c7a83
David Tang <56545687+wei0914@users.noreply.github.com>
2021-06-02 16:18:48 +0800

    Update 002. Add Two Numbers.cpp

--- 002. Add Two Numbers.cpp
+++ 002. Add Two Numbers.cpp
@@ -13,7 +13,7 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         if(!l1)
             return l2;
-        if(!l2)
+        else if(!l2)
             return l1;
         
          ListNode *head = new ListNode(0);
