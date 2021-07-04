925e6c2dadb13840be616943f55195d7de6e5aba
David Tang <56545687+wei0914@users.noreply.github.com>
2021-07-04 15:33:23 +0800

    Create 056. Merge Intervals.cpp

---
+++ 056. Merge Intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.empty())
+            return intervals;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        
+        for(int i=1; i<intervals.size(); ++i){
+            if(intervals[i][0] > ans.back()[1])
+                ans.push_back(intervals[i]);
+            else //merge
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+        return ans;
+    }
+};
