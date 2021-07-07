3206a8eb643462e06cf95d22da85d042f20fb23c
David Tang <56545687+wei0914@users.noreply.github.com>
2021-07-07 22:57:27 +0800

    Create 023. Merge k Sorted Lists.cpp

---
+++ 023. Merge k Sorted Lists.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) 
+            return NULL;
+        
+        int len = lists.size();
+        while (len > 1) {
+            for (int i = 0; i < len / 2; ++i) {
+                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
+            }
+            len = (len + 1) / 2;
+        }
+        
+        return lists.front();
+    }
+    
+    //merge
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(l1 == nullptr){
+            return l2;
+        }
+        if(l2 == nullptr){
+            return l1;
+        }
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+    
+};
+
+/*
+0,1,2,3,4   len = 5
+
+0,1,2       len = 3
+
+0           len = 1 ->stop
+
+*/
